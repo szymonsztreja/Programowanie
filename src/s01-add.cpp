3a1619c932a4ef10398aea7099222fff099b79ba
Student <student@student-VirutalBox>
2021-10-14 15:34:59 +0200

    Zadania z 1. wykładu

---
+++ src/s01-add.cpp
@@ -0,0 +1,10 @@
+# include <iostream> // for std :: cout , std :: cerr , and std :: cin
+# include <string> 
+
+auto main(int, char* argv[]) -> int
+{
+    auto const a = std :: stoi ( argv [1]);
+    auto const b = std :: stoi ( argv [2]);
+    std :: cout << ( a + b ) << "\n ";
+return 0;
+}
